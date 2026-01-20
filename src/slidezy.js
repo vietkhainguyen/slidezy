4ead68182b3cacfe50f4ada18b5e82d94ee0a18c
WindowsX <WindowsX@HPPROBOOK>
2026-01-20 19:57:58 +0700

    Add Slideby

--- src/slidezy.js
+++ src/slidezy.js
@@ -11,6 +11,7 @@ function Slidezy(selector, options = {}) {
       speed: 300,
       loop: false,
       nav: true,
+      controls: true,
       controlText: ["<", ">"],
       prevButton: null,
       nextButton: null,
@@ -30,7 +31,10 @@ Slidezy.prototype._init = function () {
 
   this._createContent();
   this._createTrack();
-  this._createControls();
+
+  if (this.opt.controls) {
+    this._createControls();
+  }
 
   if (this.opt.nav) {
     this._createNav();
@@ -68,19 +72,30 @@ Slidezy.prototype._createTrack = function () {
 };
 
 Slidezy.prototype._createControls = function () {
-  this.prevBtn = document.createElement("button");
-  this.nextBtn = document.createElement("button");
+  this.prevBtn = this.opt.prevButton
+    ? document.querySelector(this.opt.prevButton)
+    : document.createElement("button");
+  this.nextBtn = this.opt.nextButton
+    ? document.querySelector(this.opt.nextButton)
+    : document.createElement("button");
 
-  this.prevBtn.textContent = "<";
-  this.nextBtn.textContent = ">";
+  if (!this.opt.prevButton) {
+    this.prevBtn.textContent = this.opt.controlText[0];
+    this.prevBtn.className = "slidezy-prev";
+    this.content.appendChild(this.prevBtn);
+  }
 
-  this.prevBtn.className = "slidezy-prev";
-  this.nextBtn.className = "slidezy-next";
+  if (!this.opt.nextButton) {
+    this.nextBtn.textContent = this.opt.controlText[1];
+    this.nextBtn.className = "slidezy-next";
+    this.content.appendChild(this.nextBtn);
+  }
 
-  this.container.append(this.prevBtn, this.nextBtn);
+  const stepSize =
+    this.opt.slideBy === "page" ? this.opt.items : this.opt.slideBy;
 
-  this.prevBtn.onclick = () => this.moveSlide(-1);
-  this.nextBtn.onclick = () => this.moveSlide(1);
+  this.prevBtn.onclick = () => this.moveSlide(-stepSize);
+  this.nextBtn.onclick = () => this.moveSlide(stepSize);
 };
 
 Slidezy.prototype._createNav = function () {
